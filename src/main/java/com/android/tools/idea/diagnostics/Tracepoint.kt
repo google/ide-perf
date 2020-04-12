1419478e10681b65488ea58d865621b4ef94db1c
Matthew Gharrity <gharrma@google.com>
2020-04-12 18:15:18 -0400

    Add Javadoc for Tracepoint

--- src/main/java/com/android/tools/idea/diagnostics/Tracepoint.kt
+++ src/main/java/com/android/tools/idea/diagnostics/Tracepoint.kt
@@ -1,5 +1,6 @@
 package com.android.tools.idea.diagnostics
 
+/** Represents a method (usually) for which we are gathering call counts and timing information. */
 class Tracepoint(private val name: String) {
     override fun toString(): String = name
 
