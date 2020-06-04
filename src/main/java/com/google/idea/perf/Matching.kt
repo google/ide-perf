cfc7f87a0ae619a5f8a55abb20587be3dbf81d28
Josh <40677877+josh-127@users.noreply.github.com>
2020-06-04 09:45:56 -0500

    Add fuzzy matching algorithm (#11)
    This will be used for autocompletion.
    Fuzzy matching is based on the Smith-Waterman algorithm.
    Caching and improvements will be implemented in later PRs.

---
+++ src/main/java/com/google/idea/perf/Matching.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.idea.perf
+
+class MatchResult(
+    val source: String,
+    val matchedChars: List<Int>,
+    val score: Int
+)
+
+fun fuzzyMatchMany(sources: Collection<String>, pattern: String): List<MatchResult> {
+    return sources
+        .map { fuzzyMatch(it, pattern) }
+        .sortedByDescending { it.score }
+}
+
+private const val ROOT: Byte = 0
+private const val LEFT: Byte = 1
+private const val UP: Byte = 2
+private const val DIAGONAL: Byte = 3
+
+private const val MATCH_SCORE = 4
+private const val MISMATCH_SCORE = -4
+private const val GAP_SCORE = -1
+
+/**
+ * Tries to approximate the best fit substring given a string pattern. This method is based off the
+ * Smith-Waterman algorithm.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm">Smith-Waterman algorithm</a>
+ */
+fun fuzzyMatch(source: String, pattern: String): MatchResult {
+    val scoreMatrix = Array(pattern.length + 1) { IntArray(source.length + 1) }
+    val parentMatrix = Array(pattern.length + 1) { ByteArray(source.length + 1) }
+
+    // Construct score and parent matrix
+    for (r in 1..pattern.length) {
+        for (c in 1..source.length) {
+            val leftScore = scoreMatrix[r][c - 1] + GAP_SCORE
+            val upScore = scoreMatrix[r - 1][c] + GAP_SCORE
+            var diagonalScore = scoreMatrix[r - 1][c - 1]
+            diagonalScore += if (pattern[r - 1] == source[c - 1]) MATCH_SCORE else MISMATCH_SCORE
+
+            val maxScore = maxOf(leftScore, upScore, diagonalScore)
+            scoreMatrix[r][c] = maxScore
+
+            if (maxScore >= 0) {
+                when (maxScore) {
+                    diagonalScore -> parentMatrix[r][c] = DIAGONAL
+                    upScore -> parentMatrix[r][c] = UP
+                    leftScore -> parentMatrix[r][c] = LEFT
+                }
+            }
+        }
+    }
+
+    // Get maximum score
+    var row = 0
+    var column = 0
+    var maxScore = Int.MIN_VALUE
+
+    for (r in pattern.indices) {
+        for (c in source.indices) {
+            if (scoreMatrix[r][c] > maxScore) {
+                maxScore = scoreMatrix[r][c]
+                row = r
+                column = c
+            }
+        }
+    }
+
+    // Gather matched characters
+    val matchedChars = ArrayList<Int>(source.length)
+
+    while (parentMatrix[row][column] != ROOT) {
+        when (parentMatrix[row][column]) {
+            LEFT -> column--
+            UP -> row--
+            DIAGONAL -> {
+                row--
+                column--
+                if (pattern[row] == source[column]) {
+                    matchedChars.add(column)
+                }
+            }
+        }
+    }
+
+    matchedChars.reverse()
+
+    return MatchResult(source, matchedChars, maxScore)
+}
---
+++ src/test/java/com/google/idea/perf/MatchingTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.idea.perf
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+private fun assertMatch(expectMatch: Boolean, source: String, pattern: String) {
+    val match = fuzzyMatch(source, pattern)
+    if (expectMatch) {
+        assertTrue(match.score > 0)
+    }
+    else {
+        assertTrue(match.score <= 0)
+    }
+}
+
+class MatchingTest {
+    @Suppress("SpellCheckingInspection")
+    @Test
+    fun testFuzzyMatcher() {
+        assertMatch(true, "java.lang.String", "java.lang.String")
+        assertMatch(true, "java.lang.String", "javalangString")
+        assertMatch(true, "java.lang.String", "String")
+        assertMatch(true, "java.lang.String", "Strng")
+        assertMatch(true, "java.lang.String", "Str")
+        assertMatch(false, "java.lang.String", "$$$$$$$$$$")
+        assertMatch(false, "java.lang.String", "          ")
+    }
+}
