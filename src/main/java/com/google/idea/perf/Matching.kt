2c04c2250610ca0c75bb0b9fa02833774029ce80
Josh <40677877+josh-127@users.noreply.github.com>
2020-06-10 14:41:21 -0500

    Improve fuzzy search performance (#15)
    - Introduced a search cache. The cached search algorithm feeds
      previous search results back into the input.
    
    - Added unit tests and benchmarks for fuzzy search.
    
    - Added fast match for first few characters.

--- src/main/java/com/google/idea/perf/Matching.kt
+++ src/main/java/com/google/idea/perf/Matching.kt
@@ -16,6 +16,8 @@
 
 package com.google.idea.perf
 
+import com.intellij.util.containers.SLRUMap
+
 class MatchResult(val source: String, val formattedSource: String) {
     companion object {
         const val MATCHED_RANGE_OPEN_TOKEN = '{'
@@ -23,50 +25,58 @@ class MatchResult(val source: String, val formattedSource: String) {
     }
 }
 
+class FuzzySearcher {
+    private val impl = FuzzySearcherImpl()
+
+    fun search(
+        sources: Collection<String>,
+        pattern: String,
+        maxResults: Int,
+        cancellationCheck: () -> Unit
+    ): List<MatchResult> {
+        var results = impl.search(sources, pattern, cancellationCheck)
+
+        if (maxResults >= 0) {
+            results = results.take(maxResults)
+        }
+
+        return results.map {
+            val builder = StringBuilder(it.source.length * 2)
+            var isMatched = false
+            var isPrevMatched = isMatched
+
+            for ((index, char) in it.source.withIndex()) {
+                isMatched = it.matchedChars.contains(index)
+                if (!isPrevMatched && isMatched) {
+                    builder.append(MatchResult.MATCHED_RANGE_OPEN_TOKEN)
+                }
+                else if (isPrevMatched && !isMatched) {
+                    builder.append(MatchResult.MATCHED_RANGE_CLOSE_TOKEN)
+                }
+
+                builder.append(char)
+                isPrevMatched = isMatched
+            }
+
+            if (isPrevMatched) {
+                builder.append(MatchResult.MATCHED_RANGE_CLOSE_TOKEN)
+            }
+
+            MatchResult(it.source, builder.toString())
+        }
+    }
+}
+
+private val globalSearcher = FuzzySearcher()
+
 /** Searches on a list of strings based on an approximate pattern. */
 fun fuzzySearch(
     sources: Collection<String>,
     pattern: String,
+    maxResults: Int,
     cancellationCheck: () -> Unit
 ): List<MatchResult> {
-    val results = ArrayList<MatchDetails>()
-
-    for ((index, source) in sources.withIndex()) {
-        val match = fuzzyMatchImpl(source, pattern)
-        if (match.matchedChars.size >= pattern.length) {
-            results.add(match)
-        }
-
-        if (index % 256 == 0) {
-            cancellationCheck()
-        }
-    }
-
-    results.sortByDescending { it.score }
-    return results.map {
-        val builder = StringBuilder(it.source.length * 2)
-        var isMatched = false
-        var isPrevMatched = isMatched
-
-        for ((index, char) in it.source.withIndex()) {
-            isMatched = it.matchedChars.contains(index)
-            if (!isPrevMatched && isMatched) {
-                builder.append(MatchResult.MATCHED_RANGE_OPEN_TOKEN)
-            }
-            else if (isPrevMatched && !isMatched) {
-                builder.append(MatchResult.MATCHED_RANGE_CLOSE_TOKEN)
-            }
-
-            builder.append(char)
-            isPrevMatched = isMatched
-        }
-
-        if (isPrevMatched) {
-            builder.append(MatchResult.MATCHED_RANGE_CLOSE_TOKEN)
-        }
-
-        MatchResult(it.source, builder.toString())
-    }
+    return globalSearcher.search(sources, pattern, maxResults, cancellationCheck)
 }
 
 /**
@@ -80,18 +90,18 @@ fun fuzzyMatch(source: String, pattern: String): Boolean {
 }
 
 /*
- * Implementation details
+ * Fuzzy matcher implementation
  *
  * Assuming all strings are ASCII, case-insensitive matching should work fine.
  */
 
 /* Base scores */
 private const val GAP_SCORE = -1
-private const val MATCH_SCORE = -GAP_SCORE * 8
+private const val MATCH_SCORE = -GAP_SCORE * 4
 private const val MISMATCH_SCORE = -MATCH_SCORE
 
 /* Constants for super scores */
-private const val GAP_RECOVERY_SCORE = MATCH_SCORE * 2
+private const val GAP_RECOVERY_SCORE = MATCH_SCORE
 
 /*
  * Super scores
@@ -105,6 +115,12 @@ private const val DELIMITER_SCORE = MATCH_SCORE * 16
 private const val POST_DELIMITER_SCORE = MATCH_SCORE * 4
 private const val CAMEL_CASE_SCORE = GAP_RECOVERY_SCORE
 
+/*
+ * Minimum number of characters required to use smart match.
+ * Usually, users start making typos no earlier than the third character.
+ */
+private const val SMART_MATCH_MIN_LENGTH = 3
+
 private const val ROOT: Byte = 0
 private const val LEFT: Byte = 1
 private const val UP: Byte = 2
@@ -117,25 +133,80 @@ private fun charEquals(c1: Char, c2: Char) = c1.toLowerCase() == c2.toLowerCase(
 private class MatchDetails(val source: String, val matchedChars: List<Int>, val score: Int)
 
 private fun fuzzyMatchImpl(source: String, pattern: String): MatchDetails {
+    if (pattern.isEmpty()) {
+        return MatchDetails(source, emptyList(), 0)
+    }
+
+    if (pattern.length < SMART_MATCH_MIN_LENGTH) {
+        return fastMatch(source, pattern)
+    }
+
+    return smartMatch(source, pattern)
+}
+
+private fun getCharacterScore(
+    source: String, pattern: String, sourceIndex: Int, patternIndex: Int
+): Int {
+    val char = source[sourceIndex]
+    val patternChar = pattern[patternIndex]
+    val prevChar = source.getOrElse(sourceIndex - 1) { ' ' }
+    val matchesCase = char == patternChar
+
+    return when {
+        !charEquals(char, patternChar) -> MISMATCH_SCORE
+        matchesCase && patternIndex == 0 && sourceIndex == 0 -> FIRST_CHAR_SCORE
+        prevChar.isLowerCase() && char.isUpperCase() -> CAMEL_CASE_SCORE
+        isDelimiter(char) -> DELIMITER_SCORE
+        matchesCase && isDelimiter(prevChar) -> POST_DELIMITER_SCORE
+        else -> MATCH_SCORE
+    }
+}
+
+private fun fastMatch(source: String, pattern: String): MatchDetails {
+    if (pattern.length > source.length) {
+        return MatchDetails(source, emptyList(), 0)
+    }
+
+    val firstOffset = source.indexOfFirst { charEquals(it, pattern[0]) }
+    val lastOffset = source.indexOfLast { !charEquals(it, pattern[0]) }
+
+    fun getMatch(offset: Int): Pair<Int, Int> {
+        if (offset < 0) {
+            return Pair(0, 0)
+        }
+
+        var totalScore = 0
+        var numMatches = 0
+
+        while (numMatches < pattern.length && offset + numMatches < source.length) {
+            val score = getCharacterScore(source, pattern, offset + numMatches, numMatches)
+            if (score < 0) {
+                break
+            }
+            totalScore += score
+            numMatches++
+        }
+
+        return Pair(totalScore, numMatches)
+    }
+
+    val firstMatch = getMatch(firstOffset)
+    val lastMatch = getMatch(lastOffset)
+    val bestScore = maxOf(firstMatch.first, lastMatch.first)
+    val bestMatch = if (bestScore == firstMatch.first) firstMatch else lastMatch
+    val bestStart = if (bestScore == firstMatch.first) firstOffset else lastOffset
+    val matchedChars = (bestStart..(bestStart + bestMatch.second)).toList()
+
+    return MatchDetails(source, matchedChars, bestScore)
+}
+
+private fun smartMatch(source: String, pattern: String): MatchDetails {
     val scoreMatrix = Array(pattern.length + 1) { IntArray(source.length + 1) }
     val parentMatrix = Array(pattern.length + 1) { ByteArray(source.length + 1) }
 
     // Construct score and parent matrix
-    fun getMatchScore(row: Int, column: Int): Int {
-        val char = source[column - 1]
-        val patternChar = pattern[row - 1]
-        val prevChar = source.getOrElse(column - 2) { ' ' }
-        val matchesCase = char == patternChar
-
-        return when {
-            !charEquals(char, patternChar) -> MISMATCH_SCORE
-            matchesCase && row == 1 && column == 1 -> FIRST_CHAR_SCORE
-            prevChar.isLowerCase() && char.isUpperCase() -> CAMEL_CASE_SCORE
-            isDelimiter(char) -> DELIMITER_SCORE
-            matchesCase && isDelimiter(prevChar) -> POST_DELIMITER_SCORE
-            else -> MATCH_SCORE
-        }
-    }
+    fun getMatchScore(row: Int, column: Int): Int =
+        getCharacterScore(source, pattern, column - 1, row - 1)
 
     for (r in 1..pattern.length) {
         for (c in 1..source.length) {
@@ -191,4 +262,88 @@ private fun fuzzyMatchImpl(source: String, pattern: String): MatchDetails {
     matchedChars.reverse()
 
     return MatchDetails(source, matchedChars, maxScore)
+}
+
+/*
+ * Fuzzy searcher implementation.
+ */
+
+private const val PATTERN_CACHE_SIZE = 16
+private const val RESULT_CACHE_SIZE = 256
+private const val PRUNE_FACTOR = 0.78
+
+private fun fuzzySearchUncached(
+    sources: Collection<String>,
+    pattern: String,
+    cancellationCheck: () -> Unit
+): List<MatchDetails> {
+    val results = ArrayList<MatchDetails>()
+
+    for ((index, source) in sources.withIndex()) {
+        val match = fuzzyMatchImpl(source, pattern)
+        if (match.matchedChars.size >= pattern.length) {
+            results.add(match)
+        }
+
+        if (index % 256 == 0) {
+            cancellationCheck()
+        }
+    }
+
+    results.sortByDescending { it.score }
+    return results
+}
+
+private typealias ResultCache = SLRUMap<String, List<String>>
+private typealias PatternCache = SLRUMap<Collection<String>, ResultCache>
+
+private class FuzzySearcherImpl {
+    private val patternCache = PatternCache(
+        PATTERN_CACHE_SIZE / 2, PATTERN_CACHE_SIZE - PATTERN_CACHE_SIZE / 2
+    )
+    private val patternCacheLock = Any()
+
+    fun search(
+        sources: Collection<String>,
+        pattern: String,
+        cancellationCheck: () -> Unit
+    ): List<MatchDetails> {
+        var selectedSources = sources
+
+        synchronized(patternCacheLock) {
+            val cachedPatterns = patternCache[sources]
+
+            if (cachedPatterns != null) {
+                for (i in pattern.lastIndex downTo 1) {
+                    val patternPrefix = pattern.substring(0, i)
+                    val cachedResults = cachedPatterns[patternPrefix]
+                    if (cachedResults != null) {
+                        selectedSources = cachedResults
+                        break
+                    }
+                }
+            }
+        }
+
+        val results = fuzzySearchUncached(selectedSources, pattern, cancellationCheck)
+
+        synchronized(patternCacheLock) {
+            var cachedPatterns = patternCache[sources]
+            if (cachedPatterns == null) {
+                cachedPatterns = ResultCache(
+                    RESULT_CACHE_SIZE / 2, RESULT_CACHE_SIZE - RESULT_CACHE_SIZE / 2
+                )
+                patternCache.put(sources, cachedPatterns)
+            }
+
+            if (results.size.toDouble() / selectedSources.size < PRUNE_FACTOR) {
+                val cachedResults = cachedPatterns[pattern]
+                if (cachedResults == null) {
+                    cachedPatterns.put(pattern, results.map { it.source })
+                }
+            }
+        }
+
+        return results
+    }
 }
--- src/test/java/com/google/idea/perf/MatchingTest.kt
+++ src/test/java/com/google/idea/perf/MatchingTest.kt
@@ -18,21 +18,136 @@ package com.google.idea.perf
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 private fun assertMatch(expectMatch: Boolean, source: String, pattern: String) {
     assertEquals(expectMatch, fuzzyMatch(source, pattern))
 }
 
+private fun assertSearch(
+    patterns: List<String>,
+    expectedMatches: List<String>,
+    items: List<String>
+) {
+    for (pattern in patterns) {
+        val matches = fuzzySearch(items, pattern, -1) {}.map { it.source }
+        assertTrue(matches.containsAll(expectedMatches))
+    }
+}
+
+private val idePerfClasses = listOf(
+    "com.google.idea.perf.AgentLoader",
+    "com.google.idea.perf.CallTreeBuilder",
+    "com.google.idea.perf.CallTreeManager",
+    "com.google.idea.perf.TracerConfig",
+    "com.google.idea.perf.TracerController",
+    "com.google.idea.perf.TracerMethodTransformer"
+)
+
+private val javaLangClasses = listOf(
+    "java.lang.Boolean",
+    "java.lang.Byte",
+    "java.lang.Character",
+    "java.lang.Character\$Subset",
+    "java.lang.Character\$UnicodeBlock",
+    "java.lang.Class",
+    "java.lang.ClassLoader",
+    "java.lang.ClassValue",
+    "java.lang.Compiler",
+    "java.lang.Double",
+    "java.lang.Enum",
+    "java.lang.Float",
+    "java.lang.InheritableThreadLocal",
+    "java.lang.Integer",
+    "java.lang.Long",
+    "java.lang.Math",
+    "java.lang.Number",
+    "java.lang.Object",
+    "java.lang.Package",
+    "java.lang.Process",
+    "java.lang.ProcessBuilder",
+    "java.lang.ProcessBuilder\$Redirect",
+    "java.lang.Runtime",
+    "java.lang.RuntimePermission",
+    "java.lang.SecurityManager",
+    "java.lang.Short",
+    "java.lang.StackTraceElement",
+    "java.lang.StrictMath",
+    "java.lang.String",
+    "java.lang.StringBuffer",
+    "java.lang.StringBuilder",
+    "java.lang.System",
+    "java.lang.Thread",
+    "java.lang.ThreadGroup",
+    "java.lang.ThreadLocal",
+    "java.lang.Throwable",
+    "java.lang.Void"
+)
+
+private val allSampleClasses = idePerfClasses + javaLangClasses
+
+@Suppress("SpellCheckingInspection")
 class MatchingTest {
-    @Suppress("SpellCheckingInspection")
     @Test
-    fun testFuzzyMatcher() {
+    fun testFuzzyMatch() {
         assertMatch(true, "java.lang.String", "java.lang.String")
         assertMatch(true, "java.lang.String", "javalangString")
         assertMatch(true, "java.lang.String", "String")
-        assertMatch(true, "java.lang.String", "Strng")
         assertMatch(true, "java.lang.String", "Str")
+        assertMatch(true, "java.lang.String", "tring")
+        assertMatch(true, "java.lang.String", "ing")
         assertMatch(false, "java.lang.String", "$$$$$$$$$$")
         assertMatch(false, "java.lang.String", "          ")
+    }
+
+    @Test
+    fun testFuzzySearch() {
+        // Package search
+        assertSearch(
+            listOf(
+                "com.google.idea.perf.",
+                "com.google.idea.perf",
+                "com.google.idea.",
+                "com.google.idea",
+                "com.google.",
+                "com.google"
+            ),
+            idePerfClasses,
+            allSampleClasses
+        )
+
+        assertSearch(
+            listOf("java.lang.", "java.lang", "java.", "java"),
+            javaLangClasses,
+            allSampleClasses
+        )
+
+        // Class search
+        assertSearch(
+            listOf("Tracer"),
+            listOf(
+                "com.google.idea.perf.TracerConfig",
+                "com.google.idea.perf.TracerController",
+                "com.google.idea.perf.TracerMethodTransformer"
+            ),
+            allSampleClasses
+        )
+
+        assertSearch(
+            listOf(
+                "java.lang.String",
+                "String",
+                "Strin",
+                "Str",
+                "tring",
+                "ing"
+            ),
+            listOf(
+                "java.lang.String",
+                "java.lang.StringBuffer",
+                "java.lang.StringBuilder"
+            ),
+            allSampleClasses
+        )
     }
 }
