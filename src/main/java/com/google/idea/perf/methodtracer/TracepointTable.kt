9dda22720aef3106e39b97a361f9bc047cdd133e
Matthew Gharrity <gharrma@google.com>
2020-09-06 09:44:25 -0700

    Update tracepoint table incrementally
    Instead of rebuilding the table on each frame, generate proper
    TableModelEvents to update the table incrementally. Also, sort
    the tracepoints so that their order is consistent across frames.
    
    This better preserves the current row selection.

--- src/main/java/com/google/idea/perf/methodtracer/TracepointTable.kt
+++ src/main/java/com/google/idea/perf/methodtracer/TracepointTable.kt
@@ -28,6 +28,7 @@ import javax.swing.table.AbstractTableModel
 import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.JTableHeader
 import javax.swing.table.TableRowSorter
+import kotlin.math.min
 
 // Things to improve:
 // - Update column width if numbers get too large.
@@ -43,11 +44,6 @@ private const val COL_COUNT = 4
 /** The table model for [TracepointTable]. */
 class TracepointTableModel: AbstractTableModel() {
     private var data: List<TracepointStats>? = null
-
-    fun setTracepointStats(newStats: List<TracepointStats>?) {
-        data = newStats
-        fireTableDataChanged()
-    }
 
     override fun getColumnCount(): Int = COL_COUNT
 
@@ -79,6 +75,44 @@ class TracepointTableModel: AbstractTableModel() {
     }
 
     override fun isCellEditable(row: Int, column: Int): Boolean = false
+
+    fun setTracepointStats(newStats: List<TracepointStats>?) {
+        val oldStats = data
+        if (oldStats == null && newStats == null) {
+            return
+        }
+
+        if (oldStats == null || newStats == null) {
+            data = newStats
+            fireTableDataChanged()
+            return
+        }
+
+        // We try to make newStats look "similar" to oldStats by reordering its elements.
+        // That way we preserve the current row selection in the common case.
+        val newStatsMap = newStats.associateByTo(LinkedHashMap(newStats.size)) { it.tracepoint }
+        val newStatsOrdered = ArrayList<TracepointStats>(newStats.size)
+        for (oldStat in oldStats) {
+            val newStat = newStatsMap.remove(oldStat.tracepoint) ?: continue
+            newStatsOrdered.add(newStat)
+        }
+        newStatsOrdered.addAll(newStatsMap.values) // New insertions.
+        check(newStatsOrdered.size == newStats.size)
+
+        data = newStatsOrdered
+
+        // Generate table model events.
+        val oldRows = oldStats.size
+        val newRows = newStats.size
+        when {
+            newRows > oldRows -> fireTableRowsInserted(oldRows, newRows - 1)
+            newRows < oldRows -> fireTableRowsDeleted(newRows, oldRows - 1)
+        }
+        val modifiedRows = min(oldRows, newRows)
+        if (modifiedRows > 0) {
+            fireTableRowsUpdated(0, modifiedRows - 1)
+        }
+    }
 }
 
 /** Displays a list of tracepoints alongside their call counts and timing measurements. */
@@ -154,11 +188,6 @@ class TracepointTable(private val model: TracepointTableModel): JBTable(model) {
     }
 
     fun setTracepointStats(newStats: List<TracepointStats>?) {
-        // Changing table data clears the current row selection, so we have to restore it manually.
-        val selection = selectionModel.leadSelectionIndex
         model.setTracepointStats(newStats)
-        if (selection != -1 && selection < model.rowCount) {
-            selectionModel.setSelectionInterval(selection, selection)
-        }
     }
 }
