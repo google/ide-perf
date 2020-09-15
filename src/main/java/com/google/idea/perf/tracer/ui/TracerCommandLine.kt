e5c91540cefd67fa61e022f2ab8e24beba405035
Matthew Gharrity <gharrma@google.com>
2020-09-14 20:53:00 -0700

    Work around IDEA-248576
    TextCompletionProvider stops working after a while because the user
    data in its PsiFile is lost (including COMPLETING_TEXT_FIELD_KEY).
    See https://youtrack.jetbrains.com/issue/IDEA-248576.
    
    This is the workaround.

--- src/main/java/com/google/idea/perf/tracer/ui/TracerCommandLine.kt
+++ src/main/java/com/google/idea/perf/tracer/ui/TracerCommandLine.kt
@@ -16,9 +16,15 @@
 
 package com.google.idea.perf.tracer.ui
 
+import com.intellij.openapi.application.ApplicationManager.getApplication
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.event.BulkAwareDocumentListener
 import com.intellij.openapi.editor.ex.util.EditorUtil
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.util.textCompletion.TextCompletionProvider
+import com.intellij.util.textCompletion.TextCompletionUtil
 import com.intellij.util.textCompletion.TextFieldWithCompletion
 import java.awt.Dimension
 import java.awt.KeyboardFocusManager
@@ -41,14 +47,26 @@ class TracerCommandLine(
         setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, emptySet())
         setFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, emptySet())
 
-        // Register the command handler.
         addSettingsProvider { editor ->
+            // Register the command handler.
             val enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)
-            val enterHandler = ActionListener {
-                commandHandler(text)
-                text = ""
-            }
+            val enterHandler = ActionListener { commandHandler(text); text = "" }
             editor.contentComponent.registerKeyboardAction(enterHandler, enter, WHEN_FOCUSED)
+
+            // TODO: This is a workaround for IDEA-248576.
+            editor.document.addDocumentListener(object : BulkAwareDocumentListener.Simple {
+                override fun beforeDocumentChange(document: Document) {
+                    getApplication().assertReadAccessAllowed()
+                    val psiDocumentManager = PsiDocumentManager.getInstance(project)
+                    val psiFile = psiDocumentManager.getPsiFile(document) ?: return
+                    val provider = TextCompletionUtil.getProvider(psiFile)
+                    if (provider == null) {
+                        val logger = Logger.getInstance(TracerCommandLine::class.java)
+                        logger.warn("Reinstalling TextCompletionProvider (see IDEA-248576)")
+                        TextCompletionUtil.installProvider(psiFile, completionProvider, true)
+                    }
+                }
+            })
         }
     }
 }
